ce60dcaffd98b8a6f8b8f8e98148f11e8d6f9cc3
Swan Htet Aung <swanhtetaung308@gmail.com>
2025-03-22 00:08:52 +0700

    routing with dynamic component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
 import TheHeader from './components/TheHeader.vue'
 import TheNav from './components/TheNav.vue'
-import TheActivities from './pages/TheActivities.vue'
-import TheProgress from './pages/TheProgress.vue'
-import TheTimeLine from './pages/TheTimeLine.vue'
-import { PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE } from './constants'
-import { currentPage } from './router'
+import { currentPage, routes } from './router'
 </script>
 
 <template>
   <TheHeader />
   <main class="flex grow flex-col">
-    <TheTimeLine v-show="currentPage === PAGE_TIMELINE" />
-    <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
-    <TheProgress v-show="currentPage === PAGE_PROGRESS" />
+    <KeepAlive>
+      <component :is="routes[currentPage]" />
+    </KeepAlive>
   </main>
   <TheNav />
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import { ref } from 'vue'
-import { PAGE_TIMELINE } from '@/constants'
+import { PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE } from '@/constants'
 import { isPageValid } from '@/validator'
+import TheTimeLine from '@/pages/TheTimeLine.vue'
+import TheActivities from '@/pages/TheActivities.vue'
+import TheProgress from '@/pages/TheProgress.vue'
 
 export const normalizePageHash = (): string => {
   const page = window.location.hash.slice(1)
@@ -16,4 +19,10 @@ export const currentPage = ref<string>(normalizePageHash())
 export const navigate = (page: string): void => {
   document.body.scrollIntoView()
   currentPage.value = page
+}
+
+export const routes: Record<string, any> = {
+  [PAGE_TIMELINE]: TheTimeLine,
+  [PAGE_ACTIVITIES]: TheActivities,
+  [PAGE_PROGRESS]: TheProgress,
 }
